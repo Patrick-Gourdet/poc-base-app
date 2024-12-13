98b2462d535b11ca82ec66b735d009661492587e
patrick <patrickgourdet79@gmail.com>
2024-12-13 08:59:40 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Button from './components/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Button>Hello</Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/button.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+import styles from './button.module.css';
+
+interface ButtonProps {
+    children: ReactNode;
+}
+
+export default function Button({ children }: ButtonProps){
+    return <>
+    <button className={styles.primary}>{children}</button>
+    </>
+}
